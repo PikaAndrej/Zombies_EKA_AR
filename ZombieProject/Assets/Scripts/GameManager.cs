fe4ff4b477ef11742c5e91eb603a2f396b687f94
Kannych <82958977+PikaAndrej@users.noreply.github.com>
2026-02-16 12:50:06 +0200

    Reapply "added assets, zombie roll"
    This reverts commit 8e069f4441916ec7eb178bdecbc10c1795907639.

---
+++ ZombieProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] zombies;
+    public GameObject selectedZombie;
+
+    void Start()
+    {
+        selectedZombie = zombies[2];
+        
+    }
+}
