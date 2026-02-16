66980d0855711c8d39affcbc855bf5aea7fcd6a1
Kannych <82958977+PikaAndrej@users.noreply.github.com>
2026-02-16 14:52:29 +0200

    pre-homework
    Selectable zombies, Push zombies

--- ZombieProject/Assets/Scripts/GameManager.cs
+++ ZombieProject/Assets/Scripts/GameManager.cs
@@ -1,13 +1,66 @@
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector3 = UnityEngine.Vector3;
+
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] zombies;
-    public GameObject selectedZombie;
+    private GameObject selectedZombie;
+    public Vector3 selectedSize;
+    public Vector3 pushForce;
+    private InputAction next, prev, jump;
+    private int selectedIndex = 0;
+   
 
-    void Start()
+    private void Start()
     {
-        selectedZombie = zombies[2];
-        
+        next = InputSystem.actions.FindAction("NextZombie");
+        prev = InputSystem.actions.FindAction("PrevZombie");
+        jump = InputSystem.actions.FindAction("Jump");
+        SelectZombie(selectedIndex);
+    }
+
+    private void Update()
+    {
+        if (next.WasPressedThisFrame())
+        {
+            Debug.Log("Next");
+            selectedIndex++;
+            if (selectedIndex >= zombies.Length)
+                selectedIndex = 0;
+            SelectZombie(selectedIndex);
+        }
+
+        if (prev.WasPressedThisFrame())
+        {
+            Debug.Log("Prev");
+            selectedIndex--;
+            if (selectedIndex < 0)
+                selectedIndex = zombies.Length - 1;
+            SelectZombie(selectedIndex);
+        }
+
+        if (jump.WasPressedThisFrame())
+        {
+            Debug.Log("Jump");
+            Rigidbody rb = selectedZombie.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(pushForce);
+            }
+        }
+    }
+    
+    void SelectZombie(int index)
+    {
+        if (selectedZombie != null)
+        {
+            selectedZombie.transform.localScale = Vector3.one;
+        }
+        selectedZombie = zombies[index]; 
+        selectedZombie.transform.localScale = selectedSize;
+        Debug.Log("Selected: " + selectedZombie);
     }
 }
